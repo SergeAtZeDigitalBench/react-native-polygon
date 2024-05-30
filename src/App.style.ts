b6291ee22267a365eb77c31d72c908022fec2452
Serge Basangovs <serge.basangovs@reachplc.com>
2024-05-30 08:52:50 +0100

    feat: image gallery completed initially

--- src/App.style.ts
+++ src/App.style.ts
@@ -9,4 +9,23 @@ export const s = StyleSheet.create({
     paddingVertical: 10,
     textAlign: "center",
   },
+  body: {
+    flex: 6,
+  },
+  imageBox: { height: 300, marginVertical: 30 },
+  imageDelete: {
+    height: 40,
+    width: 40,
+    borderRadius: 12,
+    position: "absolute",
+    top: 15,
+    right: 15,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: { height: "100%" },
+  footer: { flex: 1, justifyContent: "center", alignItems: "center" },
+  btn: { backgroundColor: "black", padding: 30 },
+  btnTxt: { color: "white" },
 });
--- src/AppCore.tsx
+++ src/AppCore.tsx
@@ -1,12 +1,82 @@
-import React from "react";
-import { Text } from "react-native";
+import { useState } from "react";
+import { Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
+import { launchImageLibraryAsync } from "expo-image-picker";
 
 import { s } from "./App.style";
 
+interface State {
+  isLoading: boolean;
+  error: null | string;
+  data: string[];
+}
+
+const initState: State = { isLoading: false, error: null, data: [] };
+
 export const AppCore = (): JSX.Element => {
+  const [images, setImages] = useState<State>(() => initState);
+
+  const handlePickImage = async () => {
+    try {
+      setImages((current) => ({ ...current, isLoading: true, error: null }));
+      const result = await launchImageLibraryAsync();
+      if (result.canceled) {
+        setImages((current) => ({
+          ...current,
+          isLoading: false,
+        }));
+        alert("No image selected");
+        return;
+      }
+      const imageUrls = result.assets.map((current) => current.uri);
+
+      setImages((current) => ({
+        ...current,
+        isLoading: false,
+        data: [...current.data, ...imageUrls],
+      }));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "Failed to load image";
+      setImages((current) => ({
+        ...current,
+        isLoading: false,
+        error: errorMessage,
+      }));
+    }
+  };
+
+  const handleDeleteImage = (uri: string) => {
+    setImages((current) => ({
+      ...current,
+      data: current.data.filter((imgUrl) => imgUrl !== uri),
+    }));
+  };
+
   return (
     <>
-      <Text style={s.title}>My App</Text>
+      <Text style={s.title}>My favorite pictures</Text>
+      <View style={s.body}>
+        <ScrollView>
+          {images.data.map((uri) => {
+            return (
+              <View key={uri} style={s.imageBox}>
+                <Image source={{ uri }} style={s.image} />
+                <TouchableOpacity
+                  onPress={() => handleDeleteImage(uri)}
+                  style={s.imageDelete}
+                >
+                  <Text style={{ color: "white" }}>🗑️</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+      <View style={s.footer}>
+        <TouchableOpacity style={s.btn} onPress={() => handlePickImage()}>
+          <Text style={s.btnTxt}>Add picture</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
